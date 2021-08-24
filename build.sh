13e45c5ef47b9a82e399e8b79badb29b4734a651
Clément Martinache <clement@martinache.net>
2021-08-24 15:06:41 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir -p /tmp/ajc
+rm -rf /tmp/ajc/*
+echo '
+public class Main {
+public static void main(String[] args) {
+System.out.println(" Hello, World ! ") ;
+}
+}
+' > /tmp/ajc/Main.java
+javac /tmp/ajc/Main.java
+
